d22e27991a60cc6645550bbd6c5ddb0b558dc4a3
clement <clement@debian12.home>
2024-12-06 10:00:27 +0100

    Ecriture d'une fonction pour renommer les Preferred Labels identiques, test réussi

--- ontology_project/ontology_helper.py
+++ ontology_project/ontology_helper.py
@@ -6,3 +6,16 @@ def replace_nan_values(dataframe):
     dataframe['Parents'] = dataframe['Parents'].apply(lambda x: 'None' if isinstance(x, float) else x)
     return dataframe
 
+def rename_duplicates(column):
+    counts = {}
+    new_values = []
+
+    for value in column:
+        if value not in counts:
+            counts[value] = 1
+            new_values.append(value)
+        else:
+            counts[value] += 1
+            new_values.append(f'{value}_{counts[value]}')
+
+    return new_values
--- ontology_project/tests.py
+++ ontology_project/tests.py
@@ -8,12 +8,26 @@ class MyTestCase(unittest.TestCase):
                            'Preferred Label': [f'entity{i}' for i in range(1, 4)],
                            'Parents': ['http://entity2/', 'http://entity3/', np.nan]})
 
-        df_without_nan = pd.DataFrame({'Class ID': [f'http://entity{i}/' for i in range(1, 4)],
+        target_df = pd.DataFrame({'Class ID': [f'http://entity{i}/' for i in range(1, 4)],
                            'Preferred Label': [f'entity{i}' for i in range(1, 4)],
                            'Parents': ['http://entity2/', 'http://entity3/', 'None']})
 
         df_with_nan_removed = replace_nan_values(df_with_nan)
-        pd.testing.assert_frame_equal(df_without_nan, df_with_nan_removed)
+        pd.testing.assert_frame_equal(target_df, df_with_nan_removed)
+
+    def test_duplicates_renaming(self):
+        df_with_duplicates = pd.DataFrame({'Class ID': ['http://entity1/', 'http://entity2/', 'http://entity3/'],
+                           'Preferred Label': ['entity1', 'entity2', 'entity1'],
+                           'Parents': ['http://entity2/', 'http://entity3/', 'http://entity4/']})
+
+        target_df = pd.DataFrame({'Class ID': ['http://entity1/', 'http://entity2/', 'http://entity3/'],
+                           'Preferred Label': ['entity1', 'entity2', 'entity1_2'],
+                           'Parents': ['http://entity2/', 'http://entity3/', 'http://entity4/']})
+
+        df_with_duplicates_renamed = df_with_duplicates.copy()
+        df_with_duplicates_renamed['Preferred Label'] = rename_duplicates(df_with_duplicates_renamed['Preferred Label'])
+        pd.testing.assert_frame_equal(target_df, df_with_duplicates_renamed)
+
 
 
 if __name__ == '__main__':
