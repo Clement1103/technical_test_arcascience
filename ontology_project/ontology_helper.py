fa94ad44d557085e48229ea07d737885ca663120
clement <clement@debian12.home>
2024-12-06 09:44:40 +0100

    Ecriture d'une fonction pour modifier les NaN values, test au vert

--- ontology_project/ontology_helper.py
+++ ontology_project/ontology_helper.py
@@ -1,1 +1,8 @@
 import pandas as pd
+
+DIR_CSV = 'onto_x.csv'
+
+def replace_nan_values(dataframe):
+    dataframe['Parents'] = dataframe['Parents'].apply(lambda x: 'None' if isinstance(x, float) else x)
+    return dataframe
+
--- ontology_project/tests.py
+++ ontology_project/tests.py
@@ -1,9 +1,19 @@
 import unittest
-
+from ontology_helper import *
+import numpy as np
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_nan_removal(self):
+        df_with_nan = pd.DataFrame({'Class ID': [f'http://entity{i}/' for i in range(1, 4)],
+                           'Preferred Label': [f'entity{i}' for i in range(1, 4)],
+                           'Parents': ['http://entity2/', 'http://entity3/', np.nan]})
+
+        df_without_nan = pd.DataFrame({'Class ID': [f'http://entity{i}/' for i in range(1, 4)],
+                           'Preferred Label': [f'entity{i}' for i in range(1, 4)],
+                           'Parents': ['http://entity2/', 'http://entity3/', 'None']})
+
+        df_with_nan_removed = replace_nan_values(df_with_nan)
+        pd.testing.assert_frame_equal(df_without_nan, df_with_nan_removed)
 
 
 if __name__ == '__main__':
