8b2793b0e20bfd9b212a76ea2c4d40456996a7f9
clement <clement@debian12.home>
2024-12-06 14:54:42 +0100

    Modification du main pour utiliser les CLI plus proprement, et ajout d'une API avec FastAPI

--- ontology_project/main.py
+++ ontology_project/main.py
@@ -1,22 +1,48 @@
 from ontology_helper import *
+import argparse
 import pandas as pd
-from ontology_helper import sort_dictionary
 import json
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-DIR_CSV = 'onto_x.csv'
+app = FastAPI()
+class QueryRequest(BaseModel):
+    dir_csv: str = 'onto_x.csv'
+    label: str
+    n: int = 10
 
-
-if __name__ == '__main__':
-    df = pd.read_csv(DIR_CSV)
+@app.post('/query-ontology/')
+def query_ontology(request: QueryRequest):
+    df = pd.read_csv(request.dir_csv)
     df_formatted = preprocess_dataframe(df)
     empty_dict = initialize_empty_dictionary_from_df(df_formatted)
-    label = input('Type a label: ')
+    label = request.label
     onto_dict = get_ontology(label, df_formatted)
     final_dict = sort_dictionary(fill_dictionary_with_ontology_results(empty_dict, onto_dict))
 
-    n = 10
+    n = request.n
+    first_elements = {k: v for i, (k, v) in enumerate(final_dict.items()) if i < n}
+    return first_elements
+
+def main(args):
+    df = pd.read_csv(args.dir_csv)
+    df_formatted = preprocess_dataframe(df)
+    empty_dict = initialize_empty_dictionary_from_df(df_formatted)
+    onto_dict = get_ontology(args.label, df_formatted)
+    final_dict = sort_dictionary(fill_dictionary_with_ontology_results(empty_dict, onto_dict))
+
+    n = args.n
     first_elements = {k: v for i, (k, v) in enumerate(final_dict.items()) if i < n}
     print(json.dumps(first_elements, indent=0))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ontology API')
+    parser.add_argument('--dir_csv', default='onto_x.csv', type=str, help='Path to the csv file containing the ontology')
+    parser.add_argument('--label', type=str, required=True, help='Name of the entity used for the query')
+    parser.add_argument('--n', default=10, type=int, help='Number of entities shown after a query. Set by default at 10')
+
+    args = parser.parse_args()
+    main(args)
 
 
 
